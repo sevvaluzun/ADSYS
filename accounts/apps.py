ecdccd27313f29a725223fb95a3ac895cd59bb8f
Şevval Uzun <sevvalluxn@gmail.com>
2026-06-04 22:17:15 +0300

    Add admin user fixture

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,15 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-
-        username = "admin"
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="admin@adsys.com",
-                password="Admin123456!"
-            )
