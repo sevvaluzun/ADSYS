5bcbd55fe82aef0efbc87185e15c36b9e59d122d
sevvaluzun <sevvalluxn@gmail.com>
2026-06-16 20:53:28 +0300

    CartItem modelini duzelt

--- cart/models.py
+++ cart/models.py
@@ -28,3 +28,23 @@ class Cart(models.Model):
         return f"Sepet #{self.id}"
 
 
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Sepet Kalemi"
+        verbose_name_plural = "Sepet Kalemleri"
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
